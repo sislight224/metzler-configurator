460b4b2314de3f40c132557100cf209321dc1bde
Enes Tufekci <enesxtufekci@gmail.com>
2024-02-16 11:43:32 +0100

    checkout

--- src/containers/Summary/containers/FooterControls/FooterControls.tsx
+++ src/containers/Summary/containers/FooterControls/FooterControls.tsx
@@ -6,6 +6,7 @@ import usePanelsStore from "../../../../hooks/store/usePanelsStore";
 import { useAppSnackbar } from "../../../../hooks/useAppSnackbar";
 import ModelControls from "../ModelControls/ModelControls";
 import { useUndoRedoStore } from "../../../../hooks/store/useUndoRedoStore";
+import { useRouter } from "next/router";
 
 const FooterControls = observer(() => {
   const {
@@ -21,14 +22,33 @@ const FooterControls = observer(() => {
     defaultErrorMessage: "",
   });
 
+  const router = useRouter();
+
   const { cleanUndoRedo } = useUndoRedoStore();
 
   const confirmConfigurationHandler = useCallback(() => {
-    fetch("/api/checkout")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
+    const formData = new FormData();
+
+    formData.append("jtl_token", String(router.query["jtl-token"]));
+    formData.append("wlPos", "0");
+    formData.append("inWarenkorb", "1");
+    formData.append("a", "10");
+    formData.append("wke", "1");
+    formData.append("show", "1");
+    formData.append("kKundengruppe", "1");
+    formData.append("kSprache", "1");
+    formData.append("anzahl", "1");
+    formData.append("inWarenkorb", "In den Warenkorb");
+
+    fetch("http://wm-dev.de/Briefkasten-BK212-Blindmodul", {
+      method: "POST",
+      body: formData,
+    }).then((res) => {
+      if (res.ok) {
+        window.location.href = "http://wm-dev.de/Warenkorb";
+      }
+    });
+
     /* orderPanelsConfig(configId)
      *   .then(() => {
      *     setIsOrderConfig(true);
@@ -46,13 +66,29 @@ const FooterControls = observer(() => {
         <ModelControls />
       </div>
       <ConfirmConfiguration
-        onConfirm={() =>
-          fetch("/api/checkout")
-            .then((res) => res.json())
-            .then((data) => {
-              console.log(data);
-            })
-        }
+        onConfirm={() => {
+          const formData = new FormData();
+
+          formData.append("jtl_token", String(router.query["jtl-token"]));
+          formData.append("wlPos", "0");
+          formData.append("inWarenkorb", "1");
+          formData.append("a", "10");
+          formData.append("wke", "1");
+          formData.append("show", "1");
+          formData.append("kKundengruppe", "1");
+          formData.append("kSprache", "1");
+          formData.append("anzahl", "1");
+          formData.append("inWarenkorb", "In den Warenkorb");
+
+          fetch("http://wm-dev.de/Briefkasten-BK212-Blindmodul", {
+            method: "POST",
+            body: formData,
+          }).then((res) => {
+            if (res.ok) {
+              window.location.href = "http://wm-dev.de/Warenkorb";
+            }
+          });
+        }}
       />
     </div>
   );
